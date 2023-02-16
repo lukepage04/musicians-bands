ef082e42540e3e31a6f320812f6bdf57b3dd51d7
Luke Page <kelly.luke2705@gmail.com>
2023-02-16 10:19:22 +0000

    Fixed Formatting

--- Band.js
+++ Band.js
@@ -2,12 +2,13 @@ const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Band model
 class Band extends Sequelize.Model {}
-Band.init({
-name: Sequelize.STRING,
-genre: Sequelize.STRING,
-yearFormed: Sequelize.INTEGER
-}, { sequelize, modelName: 'band' });
+    Band.init({
+    name: Sequelize.STRING,
+    genre: Sequelize.STRING,
+    yearFormed: Sequelize.INTEGER
+    }, 
+{ sequelize, modelName: 'band' });
 
 module.exports = {
-Band
+    Band
 };
--- db.js
+++ db.js
@@ -3,11 +3,11 @@ const { Sequelize } = require('sequelize');
 
 // TODO - create the new sequelize connection
 const sequelize = new Sequelize({
-dialect: 'sqlite',
-storage: path.join(__dirname, 'database.sqlite')
+    dialect: 'sqlite',
+    storage: path.join(__dirname, 'database.sqlite')
 });
 
 module.exports = {
-sequelize,
-Sequelize
+    sequelize,
+    Sequelize
 };
